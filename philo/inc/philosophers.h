123931ae2f45fe43b15b2cd3d4dd719bfb4e8489
Alejandro Quintero <alejandquinteroe@gmail.com>
2024-05-24 23:37:38 +0200

    structure

--- philo/inc/philosophers.h
+++ philo/inc/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 21:11:46 by aquinter          #+#    #+#             */
-/*   Updated: 2024/05/23 23:51:26 by aquinter         ###   ########.fr       */
+/*   Updated: 2024/05/24 22:28:15 by aquinter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,37 @@
 
 typedef struct s_params
 {
-	int	n_philos;
-	int	n_times_to_eat;
+	int	number_of_philos;
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
+	int	number_of_meals;
 }	t_params;
+
+typedef struct s_philo
+{
+	pthread_t		thread;
+	int				id;
+	int				last_meal_time;
+	int				start_time;
+	int				is_dead;
+	int				is_eating;
+	int				meals_count;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*dead_mutex;
+	pthread_mutex_t	*log_mutex;
+	pthread_mutex_t	*meal_mutex;
+	t_params		*t_params;
+}	t_philo;
+
+typedef struct s_program
+{
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	dead_mutex;
+	pthread_mutex_t	log_mutex;
+	pthread_mutex_t	meal_mutex;
+}	t_program;
 
 void	print(char *msg);
 
--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 21:39:17 by aquinter          #+#    #+#             */
-/*   Updated: 2024/05/24 00:16:48 by aquinter         ###   ########.fr       */
+/*   Updated: 2024/05/24 21:50:11 by aquinter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,15 +50,15 @@ bool	is_digit(char *str)
 bool	valid_input(char *argv[])
 {
 	if (!is_digit(argv[1]) || ft_stol(argv[1]) <= 0 || ft_stol(argv[1]) > INT_MAX)
-		return(printf("!philos\n"), false);
+		return(printf("Invalid philosophers numbers\n"), false);
 	if (!is_digit(argv[2]) || ft_stol(argv[2]) <= 0 || ft_stol(argv[2]) > INT_MAX)
-		return(printf("!tdie\n"), false);
+		return(printf("Invalid time to die\n"), false);
 	if (!is_digit(argv[3]) || ft_stol(argv[3]) <= 0 || ft_stol(argv[3]) > INT_MAX)
-		return(printf("!toeat\n"), false);
+		return(printf("Invalid time to eat\n"), false);
 	if (!is_digit(argv[4]) || ft_stol(argv[4]) <= 0 || ft_stol(argv[4]) > INT_MAX)
-		return(printf("!tosleep\n"), false);
+		return(printf("Invalid time to sleep\n"), false);
 	if (argv[5] && (!is_digit(argv[5]) || ft_stol(argv[5]) < 0 || ft_stol(argv[5]) > INT_MAX))
-		return(printf("!n_to_eat\n"), false);
+		return(printf("Invalid number of times each philosopher must eat\n"), false);
 	return (true);
 }
 
@@ -66,14 +66,14 @@ bool	get_input(char *argv[], t_params *params)
 {
 	if (!valid_input(argv))
 		return (false);
-	params->n_philos = (int)ft_stol(argv[1]);
+	params->number_of_philos = (int)ft_stol(argv[1]);
 	params->time_to_die = (int)ft_stol(argv[2]);
 	params->time_to_eat = (int)ft_stol(argv[3]);
 	params->time_to_sleep = (int)ft_stol(argv[4]);
 	if (argv[5])
-		params->n_times_to_eat = (int)ft_stol(argv[5]);
+		params->number_of_meals = (int)ft_stol(argv[5]);
 	else
-		params->n_times_to_eat = -1;
+		params->number_of_meals = -1;
 	return (true);
 }
 
@@ -83,17 +83,17 @@ int	main(int argc, char *argv[])
 	t_params params;
 
 	if (argc != REQUIRED_ARGUMENTS && argc != OPTIONAL_ARGUMENTS)
-		return (printf("Invalid arguments\n"), 1);;
+		return (printf("Invalid arguments\n"), 1);
 	if (!get_input(argv, &params))
 		return	(1);
 	else
 	{
 		printf("%d\n%d\n%d\n%d\n%d\n", 
-			params.n_philos, 
+			params.number_of_philos, 
 			params.time_to_die, 
 			params.time_to_eat,
 			params.time_to_sleep,
-			params.n_times_to_eat
+			params.number_of_meals
 		);
 	}
 	return (0);
